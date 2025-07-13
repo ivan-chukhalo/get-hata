d1e9d6ad59137af720da5b6732e5c20c91b35ee2
Ivan Chukhalo <ivanchukhalo@gmail.com>
2025-07-13 21:12:11 +0300

    feat(projects): add head block for project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
       <About />
+      <Projects />
     </div>
   );
 };
---
+++ src/components/Projects.jsx
@@ -0,0 +1,20 @@
+const Projects = () => {
+  return (
+    <div
+      className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
+      id="projects"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold text-center mb-2">
+        Projects{" "}
+        <span className="underline underline-offset-4 decoration-1 font-light">
+          Completed
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto">
+        Crafting Spaces, Building Legacies-Explore Our Portfolio
+      </p>
+    </div>
+  );
+};
+
+export default Projects;

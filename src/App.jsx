87c618566b7fcb2c9841a9b281845aad8d131e90
Ivan Chukhalo <ivanchukhalo@gmail.com>
2025-07-22 00:58:59 +0300

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Projects from "./components/Projects";
 import Testimonails from "./components/Testimonails";
 import Contact from "./components/Contact";
 import { ToastContainer } from "react-toastify";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Projects />
       <Testimonails />
       <Contact />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div id="footer" className="pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden">
+      <div className="container mx-auto flex flex-col md:flex-row items-start justify-between text-white">
+        <div className="w-full md:w-1/3 mb-8 md:mb-0">
+          <img src={assets.logo_dark} alt="logo" />
+          <p className="mt-4 text-gray-400">
+            © 2025 YourCompanyName. All rights reserved. Built with passion and
+            purpose in Ukraine 🇺🇦
+          </p>
+        </div>
+        <div className="w-full md:w-1/3 mb-8 md:mb-0">
+            <h3 className="text-white text-lg font-bold mb-4">Company</h3>
+            <ul className="flex flex-col gap-2 text-gray-400">
+                <a href="#header" className="hover:text-white">Home</a>
+                <a href="#about" className="hover:text-white">About us</a>
+                <a href="#contact" className="hover:text-white">Contact us</a>
+                <a href="#" className="hover:text-white">Privacy policy</a>
+            </ul>
+        </div>
+        <div className="w-full md:w-1/3 mb-8 md:mb-0">
+            <h3 className="text-white text-lg font-bold mb-4">Subscribe to our newsletter</h3>
+            <p className="text-gray-400 mb-4 max-w-80">The latest news, articles, and resources, sent to your inbox weekly.</p>
+            <div className="flex gap-2">
+                <input type="email" placeholder="Enter your email" className="p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto"/>
+                <button className="py-2 px-4 rounded bg-blue-500 text-white">Subscribe</button>
+            </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
